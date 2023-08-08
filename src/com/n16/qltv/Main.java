2900bed90d4a18e764909443acc19cffd86942a9
GS-755 <21dh114236@st.huflit.edu.vn>
2023-08-08 14:14:20 +0700

    complete Staff feature

--- src/com/n16/qltv/Main.java
+++ src/com/n16/qltv/Main.java
@@ -1,9 +1,9 @@
 package com.n16.qltv;
 
-import com.n16.qltv.frame.staff.IndexFrame;
+import com.n16.qltv.frame.staff.LoginFrame;
 
 public class Main {
     public static void main(String[] args) {
-        IndexFrame indexFrame = new IndexFrame();
+        LoginFrame loginFrame = new LoginFrame();
     }
 }
