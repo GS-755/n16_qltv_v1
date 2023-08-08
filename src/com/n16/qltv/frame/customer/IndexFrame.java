e5b6c9d88e833e38e9c564efc9888ab4894238fa
Nhixinhdep102 <nynyennhi10022003@gmail.com>
2023-08-08 16:12:09 +0700

    Add Customer form

---
+++ src/com/n16/qltv/frame/customer/IndexFrame.java
@@ -0,0 +1,18 @@
+package com.n16.qltv.frame.customer;
+
+import javax.swing.*;
+
+public class IndexFrame {
+    private JButton thêmButton;
+    private JButton xóaButton;
+    private JButton sửaButton;
+    private JTextField textField1;
+    private JButton tìmKiếmButton;
+    private JButton cậpNhậtButton;
+    private JButton tăngDầnButton;
+    private JButton giảmDầnButton;
+    private JButton thoátButton;
+    private JTable table1;
+    private JRadioButton tuyệtĐốiRadioButton;
+    private JRadioButton tươngĐốiRadioButton;
+}
---
+++ src/com/n16/qltv/frame/customer/LoginFrame.java
@@ -0,0 +1,10 @@
+package com.n16.qltv.frame.customer;
+
+import javax.swing.*;
+
+public class LoginFrame {
+    private JTextField textField2;
+    private JCheckBox nhớMậtKhẩuCheckBox;
+    private JButton đăngNhậpButton;
+    private JTextField textField1;
+}
